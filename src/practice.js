199573cf2273cb7fad87061fef3b87cb62f539a1
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-07 19:07:13 -0400

    deleted all files, save pck json

--- src/practice.js
+++
@@ -1,5 +0,0 @@
-const prac = document.querySelector('.first-class');
-prac.addEventListener('click', ()=>{
-    const newContent = document.createTextNode("Hi there and greetings!");
-    prac.appendChild(newContent);
-})
