4b0b3459c40888b618eb213014a239c3793fd77d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-08 09:24:03 -0400

    added js functionality to btn event listener

--- colorFlipper/src/app.js
+++ colorFlipper/src/app.js
@@ -2,7 +2,10 @@ const colors = ['green', 'red', 'rgb(133,122,200', '#fi5025'];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
-console.log(Array.from(document.body.children));
-btn.addEventListener('click', ()=>{
 
+btn.addEventListener('click', ()=>{
+    const randomNumber = Math.floor(Math.random() * colors.length);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber]
 })
