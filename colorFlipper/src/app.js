4c038e40c4f89458a9ae5466b2595363aa0c1888
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-08 08:59:58 -0400

    added css stylesheet

---
+++ colorFlipper/src/app.js
@@ -0,0 +1,8 @@
+const colors = ['green', 'red', 'rgb(133,122,200', '#fi5025'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+console.log(Array.from(document.body.children));
+btn.addEventListener('click', ()=>{
+
+})
