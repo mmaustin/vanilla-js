5d4e967222bd2f3b8054792357d19be4db16de25
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-09 11:55:56 -0400

    created simple counter app files

--- colorFlipper/src/app.js
+++ colorFlipper/src/app.js
@@ -1,11 +1,24 @@
+class MakeName {
+    constructor(name){
+        this.name = name;
+    }
+}
+
+
 const colors = ['green', 'red', 'rgb(133,122,200', '#fi5025'];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+const b = document.querySelector('.bbb');
 
 btn.addEventListener('click', ()=>{
     const randomNumber = Math.floor(Math.random() * colors.length);
 
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber]
+})
+
+b.addEventListener('click', ()=> {
+    let calledClass = new MakeName('Marco')
+    document.querySelector('.yy').textContent += calledClass.name;
 })
--- colorFlipper/src/hex.js
+++ colorFlipper/src/hex.js
@@ -3,12 +3,22 @@ const hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
-btn.addEventListener('click', ()=>{
+const changeColor = () => {
     let hexColor = '#';
     for(let i = 0; i < 6; i++){
         hexColor += hex[Math.floor(Math.random() * hex.length)];
     }
 
     document.body.style.backgroundColor = hexColor;
-    color.textContent = hexColor;
-})
+    color.textContent = hexColor;    
+}
+
+btn.addEventListener('click', changeColor
+    // let hexColor = '#';
+    // for(let i = 0; i < 6; i++){
+    //     hexColor += hex[Math.floor(Math.random() * hex.length)];
+    // }
+
+    // document.body.style.backgroundColor = hexColor;
+    // color.textContent = hexColor;
+)
