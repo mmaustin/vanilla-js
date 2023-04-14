a46cdaffdaaee6e3cfd32383aa8716e6b68f334b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-14 11:13:48 -0400

    coded solution for using functionality with selectors

--- questions/app.js
+++ questions/app.js
@@ -1,10 +1,25 @@
 
 
-const btns = document.querySelectorAll('.question-btn');
+//const btns = document.querySelectorAll('.question-btn');
 //Dom traversal solution
-btns.forEach(btn => {
-    btn.addEventListener('click', e => {
-        let parent = e.currentTarget.parentElement.parentElement;
-        parent.classList.toggle('show-text');
+// btns.forEach(btn => {
+//     btn.addEventListener('click', e => {
+//         let parent = e.currentTarget.parentElement.parentElement;
+//         parent.classList.toggle('show-text');
+//     })
+// })
+
+//Selectors solution
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(question => {
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', ()=> {
+        questions.forEach(item => {
+            if(item !== question){
+                item.classList.remove('show-text')
+            }
+        })
+        question.classList.toggle('show-text');
     })
 })
