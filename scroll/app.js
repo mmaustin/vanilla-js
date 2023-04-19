81ab7ff5b1cb842afd4b4bf42d0db8006a37ce29
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-19 11:25:07 -0400

    set up display of navbar and return to top function based upon conditional heights

--- scroll/app.js
+++ scroll/app.js
@@ -6,12 +6,31 @@ const navToggle = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', () => {
     const containerHeight = linksContainer.getBoundingClientRect().height;
     const linksHeight = links.getBoundingClientRect().height;
     if(containerHeight === 0){
         linksContainer.style.height = `${linksHeight}px`;
     } else {
         linksContainer.style.height = 0;
+    }
+})
+
+const navbar = document.querySelector('#nav');
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener('scroll', ()=>{
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if(scrollHeight > navHeight){
+        navbar.classList.add('fixed-nav');
+    } else {
+        navbar.classList.remove('fixed-nav');
+    }
+
+    if(scrollHeight > 500){
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove('show-link');
     }
 })
