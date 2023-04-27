ffaf39b2d5149051fb56a45537d12a8b134027a7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-27 13:06:19 -0400

    set up delete item functionality

--- grocery/app.js
+++ grocery/app.js
@@ -91,6 +91,18 @@ function editItem(){
     console.log('edit item');
 }
 
-function deleteItem(){
-    console.log('delete item');
+function deleteItem(e){
+    const element = e.currentTarget.parentElement.parentElement;
+    const id = element.dataset.id;
+    list.removeChild(element);
+    if(list.children.length === 0){
+        container.classList.remove('show-container');
+    }
+    displayAlert('item removed', 'success');
+    setBackToDefault();
+    //removeFromLocalStorage(id);
+}
+
+function removeFromLocalStorage(id){
+
 }
