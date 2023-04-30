65a1def146bbc08399d36dc960dfca9db17fdb7c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-30 12:56:53 -0400

    set up initial image sliding functionality

--- slider/app.js
+++ slider/app.js
@@ -5,4 +5,25 @@ const prevBtn = document.querySelector('.prevBtn');
 
 slides.forEach((slide, i)=>{
     slide.style.left = `${i * 100}%`;
-})
+});
+
+let counter = 0;
+
+nextBtn.addEventListener('click', ()=>{
+    counter++;
+    carousel();
+});
+
+prevBtn.addEventListener('click', ()=>{
+    counter--;
+    carousel();
+});
+
+function carousel(){
+    if(counter === slides.length) counter = 0;
+    if (counter < 0) counter = slides.length - 1;
+
+    slides.forEach(slide => {
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    })
+}
