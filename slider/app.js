7bc862ab4a3e011dbaf08637fbaa409d83485a35
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-30 13:08:54 -0400

    created alternative set up for scrolling through pictures

--- slider/app.js
+++ slider/app.js
@@ -20,10 +20,19 @@ prevBtn.addEventListener('click', ()=>{
 });
 
 function carousel(){
-    if(counter === slides.length) counter = 0;
-    if (counter < 0) counter = slides.length - 1;
+    // if(counter === slides.length) counter = 0;
+    // if (counter < 0) counter = slides.length - 1;
+
+    if(counter < slides.length-1){
+        nextBtn.style.display = 'block';
+    } else {
+        nextBtn.style.display = 'none';
+    }
+    if (counter > 0) prevBtn.style.display = 'block';
+    else prevBtn.style.display = 'none';
 
     slides.forEach(slide => {
         slide.style.transform = `translateX(-${counter * 100}%)`;
     })
 }
+prevBtn.style.display = 'none';
