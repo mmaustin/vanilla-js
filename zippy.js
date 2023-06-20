97421f9304d2d24c72654e7fca5e1942f16acaca
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-06-20 14:31:27 -0400

    set up index html featured section initial html

--- zippy.js
+++ zippy.js
@@ -1,1 +1,7 @@
-console.log(Boolean(0));
+
+// const getProducts = async() => {
+//   const response = await fetch('https://course-api.com/javascript-store-products');
+//   const products = await response.json();
+//   console.log(products[1].fields.price);
+// }
+// getProducts();
