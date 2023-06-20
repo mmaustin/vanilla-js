092679bd1bc12ce0ad40ddae03c9180abcf5561e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-06-20 15:17:42 -0400

    set up try/catch fetch of all products and called it when dom content loaded

--- comfyStore/src/fetchProducts.js
+++ comfyStore/src/fetchProducts.js
@@ -1,5 +1,16 @@
 import { allProductsUrl } from './utils.js';
 
-const fetchProducts = async () => {};
+
+
+
+const fetchProducts = async () => {
+  try {
+    const response = await fetch(allProductsUrl);
+    const products = await response.json();
+    return products;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default fetchProducts;
--- zippy.js
+++ zippy.js
@@ -1,7 +1,11 @@
 
 // const getProducts = async() => {
-//   const response = await fetch('https://course-api.com/javascript-store-products');
-//   const products = await response.json();
-//   console.log(products[1].fields.price);
+//   try {
+//     const response = await fetch('https://course-api.com/javascript-store-products');
+//     const products = await response.json();
+//     console.log(products[1].fields.price);
+//   } catch (err) {
+//     console.log(err);
+//   }
 // }
 // getProducts();
