0dd63fd48816efcc342a4a3c9d53d83addc1040d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-07-01 13:37:46 -0400

    set up price filter functionality

--- comfyStore/src/filters/companies.js
+++ comfyStore/src/filters/companies.js
@@ -8,7 +8,18 @@ const setupCompanies = (store) => {
     .map(company => {
       return `<button class="company-btn">${company}</button>`
     }).join('');
-  
+  companiesDOM.addEventListener('click', (e) => {
+    const element = e.target;
+    if(element.classList.contains('company-btn')){
+      let newStore = [];
+      if(element.textContent === 'all'){
+        newStore = [...store];
+      } else {
+        newStore = store.filter(product => product.company === e.target.textContent);
+      }
+      display(newStore, getElement('.products-container'));
+    }
+  })
 };
 
 export default setupCompanies;
