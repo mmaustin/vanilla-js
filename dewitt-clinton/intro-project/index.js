939c7b841b650f53045e0d40c7f84037dc88a392
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-11-28 09:51:02 -0500

    added additional even listeners on the para element

--- dewitt-clinton/intro-project/index.js
+++ dewitt-clinton/intro-project/index.js
@@ -4,11 +4,19 @@ let listItem = document.querySelector('.first-input');
 let button = document.querySelector('button');
 let container = document.querySelector('.attach');
 
-button.addEventListener('click', (e) => {
-  const para = document.createElement('p');
+button.addEventListener('click', function () {
+  let para = document.createElement('p');
   para.innerHTML = listItem.value;
+  para.classList.add('para-styles');
   container.appendChild(para);
   listItem.value = '';
 
+  para.addEventListener('click', function () {
+    para.style.color = 'red';
+    para.style.textDecoration = 'line-through';
+  });
 
+  para.addEventListener('dblclick', function () {
+    para.remove();
+  })
 })
