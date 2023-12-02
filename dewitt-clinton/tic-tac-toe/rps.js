28e992f427425105993025738fdf67442e39d4c3
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-12-02 10:12:18 -0500

    created main/start and game functions

--- dewitt-clinton/tic-tac-toe/rps.js
+++ dewitt-clinton/tic-tac-toe/rps.js
@@ -2,6 +2,7 @@
 
 const userScoreDisplay = 0;
 const compScoreDisplay = 0;
+
 const userScore = document.getElementById("user-score");
 const compScore = document.getElementById("computer-score");
 const scoreBoard = document.querySelector('.score-board');
@@ -9,3 +10,22 @@ const result = document.querySelector('.result');
 const rock = document.getElementById('r');
 const paper = document.getElementById('p');
 const scissors = document.getElementById('s');
+
+function game(userChoice) {
+  console.log(userChoice);
+}
+
+function main() {
+  rock.addEventListener('click', function () {
+    game('r');
+  });
+
+  paper.addEventListener('click', function () {
+    game('p');
+  });
+
+  scissors.addEventListener('click', function () {
+    game('s');
+  });
+};
+main();
