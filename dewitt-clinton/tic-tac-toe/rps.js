77b60bc873fb738da7561a1b20ca0cd91d3a5b56
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-12-02 09:55:27 -0500

    assign html elements to variables via the document object

---
+++ dewitt-clinton/tic-tac-toe/rps.js
@@ -0,0 +1,11 @@
+
+
+const userScoreDisplay = 0;
+const compScoreDisplay = 0;
+const userScore = document.getElementById("user-score");
+const compScore = document.getElementById("computer-score");
+const scoreBoard = document.querySelector('.score-board');
+const result = document.querySelector('.result');
+const rock = document.getElementById('r');
+const paper = document.getElementById('p');
+const scissors = document.getElementById('s');
