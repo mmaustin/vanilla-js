945c5827669c1716fc16e9bf7f53334b544ef7f3
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-12-02 10:35:20 -0500

    used math floor and random to get a random computer choice

--- dewitt-clinton/tic-tac-toe/rps.js
+++ dewitt-clinton/tic-tac-toe/rps.js
@@ -11,6 +11,11 @@ const rock = document.getElementById('r');
 const paper = document.getElementById('p');
 const scissors = document.getElementById('s');
 
+function getComputerChoice() {
+  const choices = ['r', 'p', 's'];
+  console.log(choices[Math.floor(Math.random() * choices.length)]);
+}
+
 function game(userChoice) {
   console.log(userChoice);
 }
@@ -18,14 +23,17 @@ function game(userChoice) {
 function main() {
   rock.addEventListener('click', function () {
     game('r');
+    getComputerChoice();
   });
 
   paper.addEventListener('click', function () {
     game('p');
+    getComputerChoice();
   });
 
   scissors.addEventListener('click', function () {
     game('s');
+    getComputerChoice();
   });
 };
 main();
